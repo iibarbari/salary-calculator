7fae2846e702efad445c8b7dad5ffbfe64d90e91
D0lknur CltanD1r <45620986+iibarbari@users.noreply.github.com>
2021-09-17 14:39:16 +0300

    fix currency parsing

--- helpers/formatCurrency.ts
+++ helpers/formatCurrency.ts
@@ -1,5 +1,5 @@
 export default function formatCurrency(value: number, currency: string) {
-  return new Intl.NumberFormat('en-IN', {
+  return new Intl.NumberFormat('en', {
     style: 'currency',
     currency,
   }).format(value);
